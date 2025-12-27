42ad348829f6570330f8c644c8f9d2a5d022ff2f
HossamGezo <ha2ghossam10@gmail.com>
2025-12-27 12:00:00 +0200

    Solve: [G] Palindrome Array

---
+++ 01_Training/Level_01/05_Sheet_03_Arrays/G_Palindrome_Array.cpp
@@ -0,0 +1,30 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: G Palindrome Array
+ * Source: Assiut University Training - Sheet #3
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/G
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+
+  cin >> n;
+
+  int arr[n];
+
+  for (int i = 0; i < n; ++i) cin >> arr[i];
+
+  for (int i = 0; i < n / 2; ++i) {
+    if (arr[i] != arr[(n - 1) - i]) {
+      cout << "NO";
+      return 0;
+    }
+  }
+
+  cout << "YES";
+
+  return 0;
+}
