07c9f1b0215a698cc9957cee23ca3e0e350fd2e7
HossamGezo <ha2ghossam10@gmail.com>
2026-03-21 00:10:22 +0200

    Solve: [H] Two numbers

---
+++ 01_Training/Level_01/01_Assiut_Sheet_01/H_Two_numbers.cpp
@@ -0,0 +1,22 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: H_Two_numbers
+ * Source: Assiut University Training - Sheet #1
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158
+ */
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+  double a, b;
+
+  cin >> a >> b;
+
+  cout << "floor " << a << " / " << b << " = " <<  floor(a / b) << endl;
+  cout << "ceil " << a << " / " << b <<  " = " << ceil(a / b) << endl;
+  cout << "round " << a << " / " << b << " = " << round(a / b);
+
+  return 0;
+}
