25f7141a95a2696ec53f0b4bc975fc70ddaf02a3
HossamGezo <ha2ghossam10@gmail.com>
2026-03-20 13:51:45 +0200

    Solve: [C] Simple Calculator

---
+++ 01_Training/Level_01/01_Assiut_Sheet_01/C_Simple_Calculator.cpp
@@ -0,0 +1,21 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: C_Simple_Calculator
+ * Source: Assiut University Training - Sheet #1
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  long long x, y;
+
+  cin >> x >> y;
+
+  cout << x << " + " << y << " = " << x + y << endl;
+  cout << x << " * " << y << " = " << x * y << endl;
+  cout << x << " - " << y << " = " << x - y << endl;
+
+  return 0;
+}
