fc1e468bf030e93321fae00d4cc894c4cb937d22
HossamGezo <ha2ghossam10@gmail.com>
2026-03-20 05:58:57 +0200

    Solve: [B] Basic Data Types

---
+++ 01_Training/Level_01/01_Assiut_Sheet_01/B_Basic_Data_Types.cpp
@@ -0,0 +1,27 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: B_Basic_Data_Types
+ * Source: Assiut University Training - Sheet #1
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x;
+  long long ll;
+  char ch;
+  float fl;
+  double dbl;
+
+  cin >> x >> ll >> ch >> fl >> dbl;
+
+  cout << x << "\n"
+      << ll << "\n"
+      << ch << "\n"
+      << fl << "\n"
+      << dbl;
+      
+  return 0;
+}
