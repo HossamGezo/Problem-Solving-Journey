822aa828fe66f2669ec981bee9e090c9889be14a
HossamGezo <ha2ghossam10@gmail.com>
2026-03-21 07:33:12 +0200

    Solve: [O] Calculator

---
+++ 01_Training/Level_01/01_Sheet_01_Data_type_Conditions/O_Calculator.cpp
@@ -0,0 +1,28 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: O_Calculator
+ * Source: Assiut University Training - Sheet #1
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158
+ */
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+  long long a, b;
+  char s;
+
+  cin >> a >> s >> b;
+
+  if (s == '+')
+    cout << a + b;
+  else if (s == '-')
+    cout << a - b;
+  else if (s == '*')
+    cout << a * b;
+  else
+    cout << a / b;
+
+  return 0;
+}
