e9879155f854d00b981867168f8e087158584a51
HossamGezo <ha2ghossam10@gmail.com>
2026-03-22 14:40:47 +0200

    Solve: [Y] The last 2 digits

---
+++ 01_Training/Level_01/01_Sheet_01_Data_type_Conditions/Y_The_last_2_digits.cpp
@@ -0,0 +1,24 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: Y_The_last_2_digits
+ * Source: Assiut University Training - Sheet #1
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219158
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  long long a, b, c, d, m;
+
+  cin >> a >> b >> c >> d;
+
+  m = ((a % 100) * (b % 100) * (c % 100) * (d % 100)) % 100;
+
+  if (m > 10)
+    cout << m;
+  else
+    cout << "0" << m;
+
+  return 0;
+}
