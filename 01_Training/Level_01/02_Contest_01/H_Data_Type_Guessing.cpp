d5585354577e6ef4cefa5e2752f337bd388e4109
HossamGezo <ha2ghossam10@gmail.com>
2026-03-23 08:41:53 +0200

    Solve: [H] Data Type Guessing

---
+++ 01_Training/Level_01/02_Contest_01/H_Data_Type_Guessing.cpp
@@ -0,0 +1,27 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: H_Data_Type_Guessing
+ * Source: Assiut University Training - Contest #1
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/H
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  long long n, k, a, r;
+
+
+  cin >> n >> k >> a;
+
+  r = n * k / a;
+
+  if (((n * k) % a))
+    cout << "double";
+  else if (r <= 2147483647)
+    cout << "int";
+  else
+    cout << "long long";
+
+  return 0;
+}
