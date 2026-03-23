e98524b302f0d74dc299d3d5ca3589e0cca0f421
HossamGezo <ha2ghossam10@gmail.com>
2026-03-23 05:04:39 +0200

    Solve: [E] Interval Sweep

---
+++ 01_Training/Level_01/02_Contest_01/E_Interval_Sweep.cpp
@@ -0,0 +1,22 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: E_Interval_Sweep
+ * Source: Assiut University Training - Contest #1
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/E
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+
+  cin >> a >> b;
+
+  if (a + b == 0 || a - b >= 2 || b - a >= 2)
+    cout << "NO";
+  else
+    cout << "YES";
+
+  return 0;
+}
