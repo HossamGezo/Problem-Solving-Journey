efff2a6acfb03c391f64a255296d08dd54ddd2c7
HossamGezo <ha2ghossam10@gmail.com>
2026-03-24 20:21:48 +0200

    Solve: [N] Numbers Histogram

---
+++ 01_Training/Level_01/03_Sheet_02_Loops/N_Numbers_Histogram.cpp
@@ -0,0 +1,26 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: N Numbers Histogram
+ * Source: Assiut University Training - Sheet #2
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/N
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  char s;
+  int n, m;
+
+  cin >> s >> n;
+
+  for (int i = 0; i < n; ++i) {
+    cin >> m;
+
+    for (int j = 1; j <= m; ++j) cout << s;
+
+    cout << endl;
+  }
+
+  return 0;
+}
