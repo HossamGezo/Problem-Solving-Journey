f7bf57f7907e89b9eff90e0e22b2be081d829da4
HossamGezo <ha2ghossam10@gmail.com>
2026-03-24 10:03:23 +0200

    Solve: [B] Even Numbers

---
+++ 01_Training/Level_01/03_Sheet_02_Loops/B_Even_Numbers.cpp
@@ -0,0 +1,26 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: B Even Numbers
+ * Source: Assiut University Training - Sheet #2
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/B
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+
+  cin >> n;
+
+  if (n == 1) {
+    cout << -1;
+    return 0;
+  }
+
+  for (int i = 2; i <= n; i += 2) {
+    cout << i << endl;
+  }
+
+  return 0;
+}
