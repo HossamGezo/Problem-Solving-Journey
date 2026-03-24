e9113bed1fcaec9788a62121de1f6b4be13fd526
HossamGezo <ha2ghossam10@gmail.com>
2026-03-24 20:30:54 +0200

    Solve: [O] Pyramid

---
+++ 01_Training/Level_01/03_Sheet_02_Loops/O_Pyramid.cpp
@@ -0,0 +1,23 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: O Pyramid
+ * Source: Assiut University Training - Sheet #2
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/O
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+
+  cin >> n;
+
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= i; ++j) cout << "*";
+
+    cout << endl;
+  }
+
+  return 0;
+}
