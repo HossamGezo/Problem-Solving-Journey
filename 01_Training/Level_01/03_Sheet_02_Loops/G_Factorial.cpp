9de6e93c97c601a28b339b89b60d539f7c63927e
HossamGezo <ha2ghossam10@gmail.com>
2026-03-24 12:54:47 +0200

    Solve: [G] Factorial

---
+++ 01_Training/Level_01/03_Sheet_02_Loops/G_Factorial.cpp
@@ -0,0 +1,27 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: G Factorial
+ * Source: Assiut University Training - Sheet #2
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/G
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, m;
+  long long sum;
+
+  cin >> n;
+
+  while (n--) {
+    cin >> m;
+    sum = 1;
+    for (int i = 1; i <= m; ++i) {
+      sum *= i;
+    }
+    cout << sum << endl;
+  }
+
+  return 0;
+}
