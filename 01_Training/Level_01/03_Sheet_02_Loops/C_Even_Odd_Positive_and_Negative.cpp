9f73d6e382b671a3ae4c5402e1589ce33cbf144a
HossamGezo <ha2ghossam10@gmail.com>
2026-03-24 10:57:01 +0200

    Solve: [C] Even, Odd, Positive and Negative

---
+++ 01_Training/Level_01/03_Sheet_02_Loops/C_Even_Odd_Positive_and_Negative.cpp
@@ -0,0 +1,40 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: C Even Odd Positive and Negative
+ * Source: Assiut University Training - Sheet #2
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/C
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, m, ce, co, cp, cn;
+
+  ce = co = cp = cn = 0;
+
+  cin >> n;
+
+  for (int i = 0; i < n; ++i) {
+    cin >> m;
+
+    if (m % 2 == 0)
+      ++ce;
+    else
+      ++co;
+
+    if (m == 0) continue;
+
+    if (m > 0)
+      ++cp;
+    else
+      ++cn;
+  }
+
+  cout << "Even: " << ce << endl
+       << "Odd: " << co << endl
+       << "Positive: " << cp << endl
+       << "Negative: " << cn;
+
+  return 0;
+}
