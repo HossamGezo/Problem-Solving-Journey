4b3e02ca767c5e523cccb073b42edabe8fb98b50
HossamGezo <ha2ghossam10@gmail.com>
2026-03-25 07:06:50 +0200

    Solve: [R] Sequence of Numbers and Sum

---
+++ 01_Training/Level_01/03_Sheet_02_Loops/R_Sequence_of_Numbers_and_Sum.cpp
@@ -0,0 +1,34 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: R Sequence of Numbers and Sum
+ * Source: Assiut University Training - Sheet #2
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/R
+ */
+
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, m, mn, mx, sum;
+
+  while (true) {
+    cin >> n >> m;
+
+    mn = min(n, m);
+    mx = max(n, m);
+
+    if (n <= 0 || m <= 0) break;
+
+    sum = 0;
+    for (int i = mn; i <= mx; ++i) {
+      cout << i << " ";
+
+      sum += i;
+    }
+
+    cout << "sum =" << sum << endl;
+  }
+
+  return 0;
+}
