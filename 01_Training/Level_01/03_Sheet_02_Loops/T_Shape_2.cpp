075d7628e6f1e0b8e6e63a014f92eaa9516e0689
HossamGezo <ha2ghossam10@gmail.com>
2026-03-25 07:57:32 +0200

    Solve: [T] Shape2

---
+++ 01_Training/Level_01/03_Sheet_02_Loops/T_Shape_2.cpp
@@ -0,0 +1,25 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: T Shape 2
+ * Source: Assiut University Training - Sheet #2
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/T
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+
+  cin >> n;
+
+  for (int i = 1; i <= n; ++i) {
+    for (int j = 1; j <= n - i; ++j) cout << " ";
+
+    for (int k = 1; k <= (i * 2) - 1; ++k) cout << "*";
+
+    cout << endl;
+  }
+
+  return 0;
+}
