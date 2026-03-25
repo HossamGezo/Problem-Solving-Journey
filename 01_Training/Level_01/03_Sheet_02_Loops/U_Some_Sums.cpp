d4dbd025ed8a116ab551b5057c906244fa373b18
HossamGezo <ha2ghossam10@gmail.com>
2026-03-25 10:01:44 +0200

    Solve: [U] Some Sums

---
+++ 01_Training/Level_01/03_Sheet_02_Loops/U_Some_Sums.cpp
@@ -0,0 +1,35 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: U Some Sums
+ * Source: Assiut University Training - Sheet #2
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/U
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, a, b, t, r, sum, total = 0;
+
+  cin >> n >> a >> b;
+
+  for (int i = 1; i <= n; ++i) {
+    t = i;
+    sum = 0;
+    while (t) {
+      r = t % 10;
+
+      sum += r;
+
+      t /= 10;
+    }
+
+    if (sum >= a && sum <= b) {
+      total += i;
+    }
+  }
+
+  cout << total;
+
+  return 0;
+}
