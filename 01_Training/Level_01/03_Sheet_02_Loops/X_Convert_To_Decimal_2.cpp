3f4464b6e7a0765e17ba88582ebc301f7e38c9e2
HossamGezo <ha2ghossam10@gmail.com>
2026-03-25 11:33:12 +0200

    Solve: [X] Convert To Decimal 2

---
+++ 01_Training/Level_01/03_Sheet_02_Loops/X_Convert_To_Decimal_2.cpp
@@ -0,0 +1,35 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: X Convert To Decimal 2
+ * Source: Assiut University Training - Sheet #2
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/X
+ */
+
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int t;
+  long long n, m, sum, total;
+
+  cin >> t;
+
+  while (t--) {
+    cin >> n;
+
+    m = n;
+    sum = 0;
+    while (n) {
+      if (n % 2 != 0) sum++;
+
+      n /= 2;
+    }
+
+    total = 0;
+    for (int i = 0; i < sum; ++i) total += pow(2, i);
+
+    cout << total << endl;
+  }
+  return 0;
+}
