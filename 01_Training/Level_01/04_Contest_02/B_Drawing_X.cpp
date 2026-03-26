c062b52b1019b402140472353e0c2f9413794efe
HossamGezo <ha2ghossam10@gmail.com>
2026-03-26 08:49:07 +0200

    Solve: [B] Drawing "X"

---
+++ 01_Training/Level_01/04_Contest_02/B_Drawing_X.cpp
@@ -0,0 +1,38 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: B Drawing X
+ * Source: Assiut University Training - Contest #2
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/326907/problem/B
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, s, e;
+
+  cin >> n;
+
+  s = 0;
+  e = n - 1;
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      if (s == e && (j == n / 2))
+        cout << "X";
+      else if (j == s)
+        cout << "\\";
+      else if (j == e)
+        cout << "/";
+      else
+        cout << "*";
+    }
+
+    ++s;
+    --e;
+
+    cout << endl;
+  }
+
+  return 0;
+}
