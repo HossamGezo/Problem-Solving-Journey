f08dd81e7e64186a84fa66f77156e7ad5e68193a
HossamGezo <ha2ghossam10@gmail.com>
2026-03-26 13:49:30 +0200

    Solve: [F] Adding Bits

---
+++ 01_Training/Level_01/04_Contest_02/F_Break_Number.cpp
@@ -0,0 +1,36 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: F Break Number
+ * Source: Assiut University Training - Contest #2
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/326907/problem/F
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int t;
+  long long n, sum, mx = 0;
+
+  cin >> t;
+
+  while (t--) {
+    cin >> n;
+
+    sum = 0;
+    while (n) {
+      if (n % 2 == 0)
+        ++sum;
+      else
+        break;
+
+      n /= 2;
+    }
+
+    if (sum > mx) mx = sum;
+  }
+
+  cout << mx;
+
+  return 0;
+}
