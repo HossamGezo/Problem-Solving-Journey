51e38628a11f4eb402901cd898ff3754bc8a7025
HossamGezo <ha2ghossam10@gmail.com>
2026-03-27 08:06:21 +0200

    Solve: [H] Simple Modulo

---
+++ 01_Training/Level_01/04_Contest_02/H_Simple_Mod.cpp
@@ -0,0 +1,19 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: H Simple Mod
+ * Source: Assiut University Training - Contest #2
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/326907/problem/H
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+
+  cin >> n;
+
+  cout << "0 0";
+
+  return 0;
+}
