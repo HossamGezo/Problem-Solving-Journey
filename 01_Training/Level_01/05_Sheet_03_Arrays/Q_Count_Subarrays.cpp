739d3c44b368127b54ec6f2722193b50f293ab8e
HossamGezo <ha2ghossam10@gmail.com>
2026-03-29 09:36:31 +0200

    Solve: [Q] Count Subarrays

---
+++ 01_Training/Level_01/05_Sheet_03_Arrays/Q_Count_Subarrays.cpp
@@ -0,0 +1,42 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: Q Count Subarrays
+ * Source: Assiut University Training - Sheet #3
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/Q
+ */
+
+#include <iostream>
+#include <limits>
+using namespace std;
+
+int main() {
+  int t, n, mx, cn = 0;
+
+  cin >> t;
+
+  while (t--) {
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; ++i) cin >> arr[i];
+
+    cn = 0;
+
+    for (int i = 0; i < n; ++i) {
+      mx = INT_MIN;
+
+      for (int j = i; j < n; ++j) {
+        if (mx < arr[j]) {
+          mx = arr[j];
+          ++cn;
+        } else
+          break;
+      }
+    }
+
+    cout << cn << endl;
+  }
+
+  return 0;
+}
