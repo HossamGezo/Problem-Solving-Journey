1897d8864faad3807e05a23e91e5cc5fecdb2113
HossamGezo <ha2ghossam10@gmail.com>
2026-03-30 07:35:35 +0200

    Solve: [U] Is B a subsequence of A?

---
+++ 01_Training/Level_01/05_Sheet_03_Arrays/U_Is_B_a_subsequence_of_A.cpp
@@ -0,0 +1,55 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: U Is B a subsequence of A
+ * Source: Assiut University Training - Sheet #3
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/U
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, m;
+
+  cin >> n >> m;
+
+  int a[n], b[m];
+
+  for (int i = 0; i < n; ++i) cin >> a[i];
+
+  for (int i = 0; i < m; ++i) cin >> b[i];
+
+  // int f = 0, cn = 0
+
+  // for (int i = 0; i < m; ++i) {
+  //   for (int j = f; j < n; ++j) {
+  //     if (b[i] == a[j]) {
+  //       f = j + 1;
+  //       ++cn;
+  //       break;
+  //     }
+  //   }
+  // }
+
+  // if (cn == m)
+  //   cout << "YES";
+  // else
+  //   cout << "NO";
+
+  // --- Another Solution Note: "Simpler solution"
+
+  int idx = 0;
+
+  for (int i = 0; i < n; ++i) {
+    if (a[i] == b[idx]) ++idx;
+
+    if (idx == m) break;
+  }
+
+  if (idx == m)
+    cout << "YES";
+  else
+    cout << "NO";
+
+  return 0;
+}
