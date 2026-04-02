e3a528cf82717d9ad75b77ffeba0f9434a23a39b
HossamGezo <ha2ghossam10@gmail.com>
2026-04-02 18:14:52 +0200

    Solve: [A] Create A New String

---
+++ 01_Training/Level_01/07_Sheet_04_Strings/A_Create_A_New_String.cpp
@@ -0,0 +1,22 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: A Create A New String
+ * Source: Assiut University Training - Sheet #4
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/A
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string s, t;
+
+  cin >> s >> t;
+
+  cout << s.length() << " " << t.length() << endl;
+
+  cout << s << " " << t << endl;
+
+  return 0;
+}
