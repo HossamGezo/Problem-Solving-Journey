a23a27867e50876b83c80f413c8e452da179d8ac
HossamGezo <ha2ghossam10@gmail.com>
2026-04-03 10:50:54 +0200

    Solve: [G] Conversion

---
+++ 01_Training/Level_01/07_Sheet_04_Strings/G_Conversion.cpp
@@ -0,0 +1,29 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: G Conversion
+ * Source: Assiut University Training - Sheet #4
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/G
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string s;
+
+  cin >> s;
+
+  for (int i = 0; i < s.length(); ++i) {
+    if (s[i] == ',')
+      s[i] = ' ';
+    else if (s[i] < 'a')
+      s[i] += 32;
+    else
+      s[i] -= 32;
+  }
+
+  cout << s;
+
+  return 0;
+}
