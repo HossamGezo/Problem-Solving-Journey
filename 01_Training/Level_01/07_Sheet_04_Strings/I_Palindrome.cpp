27f7ca60958bd18f9a3ab60972721f8bdcbfea5f
HossamGezo <ha2ghossam10@gmail.com>
2026-04-03 13:17:17 +0200

    Solve: [I] Palindrome

---
+++ 01_Training/Level_01/07_Sheet_04_Strings/I_Palindrome.cpp
@@ -0,0 +1,42 @@
+/**
+ * Author: Hossam (HossamGezo)
+ * Problem: I Palindrome
+ * Source: Assiut University Training - Sheet #4
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/I
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string s;
+
+  cin >> s;
+
+  // bool f = true;
+
+  // for (int i = 0; i < s.length() / 2; ++i)
+  //   if (s[i] != s[s.length() - 1 - i]) f = false;
+
+  // if (f)
+  //   cout << "YES";
+  // else
+  //   cout << "NO";
+
+  // NOTE --- Another Solution Using ' #include <algorithm> ' library "Simpler solution"
+
+  string m;
+
+  m = s;
+
+  reverse(s.begin(), s.end());
+
+  if (s == m)
+    cout << "YES";
+  else
+    cout << "NO";
+
+  return 0;
+}
