419332ea7c64957dae7388ae36ab976dff94d9d6
HossamGezo <ha2ghossam10@gmail.com>
2026-04-10 18:53:48 +0200

    Solve: [A] Add

---
+++ 01_Training/Level_01/08_Sheet_05_Functions/A_Add.cpp
@@ -0,0 +1,24 @@
+/**
+ * Author: Gezo
+ * Problem: A Add
+ * Source: Assiut University Training - Sheet #4
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/A
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void summation(int x, int y) {
+  cout << x + y;
+}
+
+int main() {
+  int x, y;
+
+  cin >> x >> y;
+
+  summation(x, y);
+
+  return 0;
+}
