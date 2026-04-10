ad0b8223dbb4009463f2eac07db90f7e9edef7d7
HossamGezo <ha2ghossam10@gmail.com>
2026-04-10 18:54:14 +0200

    Solve: [C] Wonderful Number

---
+++ 01_Training/Level_01/08_Sheet_05_Functions/C_Wonderful_Number.cpp
@@ -0,0 +1,44 @@
+/**
+ * Author: Gezo
+ * Problem: C Wonderful Number
+ * Source: Assiut University Training - Sheet #4
+ * Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/C
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool is_odd(int n) {
+  return n % 2;
+}
+
+bool is_palindrome(int n) {
+  string b = "", s = "";
+
+  while (n) {
+    b += n % 2;
+
+    n /= 2;
+  }
+
+  s = b;
+
+  reverse(s.begin(), s.end());
+
+  return s == b;
+}
+
+int main() {
+  int n;
+
+  cin >> n;
+
+  if (is_odd(n) && is_palindrome(n))
+    cout << "YES";
+  else
+    cout << "NO";
+
+  return 0;
+}
